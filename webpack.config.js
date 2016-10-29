fdc4a0ea51403af04980b01c910ef1544e6564aa
Tommaso Armstrong <tomm@tomma.so>
2016-10-29 12:26:20 +1100

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+var webpack = require('webpack');
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+var ROOT_PATH = path.resolve(__dirname);
+var APP_PATH = path.resolve(ROOT_PATH, 'src');
+var BUILD_PATH = path.resolve(ROOT_PATH, 'dist');
+
+module.exports = {
+    context: APP_PATH,
+    entry: [
+        "babel-polyfill",
+        "./index.js"
+    ],
+    output: {
+        path: BUILD_PATH,
+        publicPath: process.env.NODE_ENV === 'production' ? "" : "http://localhost:8080/app/",
+        filename: "bundle.js"
+    },
+    devtool: 'source-map',
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/,
+                loaders: ["style", "css", "sass"]
+            }
+        ]
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: "./index.ejs"
+    })]
+};
